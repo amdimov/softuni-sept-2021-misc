61e102b98418837f57ae6a9bc9539f9386aa8294
luchob <lachezar.balev@gmail.com>
2021-10-26 21:49:32 +0300

    Initial implementation of the books API. TODO: fix the delete method.

--- books/src/main/java/bg/softuni/books/web/BooksController.java
+++ books/src/main/java/bg/softuni/books/web/BooksController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,7 +60,8 @@ public class BooksController {
         build();
   }
 
-  @PutMapping()
+  //My mistake :P
+  @PostMapping()
   public ResponseEntity<BookDTO> create(
       @RequestBody BookDTO bookDTO,
       UriComponentsBuilder builder) {
