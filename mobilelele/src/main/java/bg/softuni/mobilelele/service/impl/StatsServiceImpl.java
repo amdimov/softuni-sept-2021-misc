0b88f50491b18169ca6d97e890686980b57ef8b8
luchob <lachezar.balev@gmail.com>
2021-11-02 17:33:20 +0200

    More security

--- mobilelele/src/main/java/bg/softuni/mobilelele/service/impl/StatsServiceImpl.java
+++ mobilelele/src/main/java/bg/softuni/mobilelele/service/impl/StatsServiceImpl.java
@@ -2,6 +2,7 @@ package bg.softuni.mobilelele.service.impl;
 
 import bg.softuni.mobilelele.model.view.StatsView;
 import bg.softuni.mobilelele.service.StatsService;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -13,13 +14,12 @@ public class StatsServiceImpl implements StatsService {
 
   @Override
   public void increaseRequests() {
-    Object principal =
+    Authentication authentication =
         SecurityContextHolder.
             getContext().
-            getAuthentication().
-            getPrincipal();
+            getAuthentication();
 
-    if (principal instanceof UserDetails) {
+    if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
       authenticatedRequests++;
     } else {
       anonymousRequests++;
