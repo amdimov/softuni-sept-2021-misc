ce422ec7fdd611890e56906662bd0ea15cab8292
rancho83a <rancho83a@gmail.com>
2021-11-09 01:05:51 +0200

    OrderMapper with default methos in interface

--- hateoas/src/main/java/bg/softuni/hateoas/web/StudentsController.java
+++ hateoas/src/main/java/bg/softuni/hateoas/web/StudentsController.java
@@ -7,12 +7,14 @@ import bg.softuni.hateoas.model.dto.OrderDTO;
 import bg.softuni.hateoas.model.dto.StudentDTO;
 import bg.softuni.hateoas.model.entity.OrderEntity;
 import bg.softuni.hateoas.model.entity.StudentEntity;
+import bg.softuni.hateoas.model.mapping.OrderMapper;
 import bg.softuni.hateoas.model.mapping.StudentMapper;
 import bg.softuni.hateoas.repository.StudentsRepository;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.swing.text.html.parser.Entity;
+
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
@@ -27,87 +29,93 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentsController {
 
-  private final StudentsRepository studentsRepository;
-  private final StudentMapper studentMapper;
+    private final StudentsRepository studentsRepository;
+    private final StudentMapper studentMapper;
+    private final OrderMapper orderMapper;
 
-  public StudentsController(StudentsRepository studentsRepository,
-      StudentMapper studentMapper) {
-    // WARNING: Normally we never inject repos in the controllers,
-    // we do this just for test and fun here.
-    this.studentsRepository = studentsRepository;
-    this.studentMapper = studentMapper;
-  }
+    public StudentsController(StudentsRepository studentsRepository,
+                              StudentMapper studentMapper, OrderMapper orderMapper) {
+        // WARNING: Normally we never inject repos in the controllers,
+        // we do this just for test and fun here.
+        this.studentsRepository = studentsRepository;
+        this.studentMapper = studentMapper;
+        this.orderMapper = orderMapper;
+    }
 
-  @GetMapping
-  public ResponseEntity<CollectionModel<EntityModel<StudentDTO>>> getStudents() {
-    List<EntityModel<StudentDTO>> allStudents = studentsRepository.
-        findAll().
-        stream().
-        map(studentMapper::mapEntityToDTO).
-        map(dto -> EntityModel.of(dto, createStudentLinks(dto))).
-        collect(Collectors.toList());
+    @GetMapping
+    public ResponseEntity<CollectionModel<EntityModel<StudentDTO>>> getStudents() {
+        List<EntityModel<StudentDTO>> allStudents = studentsRepository.
+                findAll().
+                stream().
+                map(studentMapper::mapEntityToDTO).
+                map(dto -> EntityModel.of(dto, createStudentLinks(dto))).
+                collect(Collectors.toList());
 
-    return ResponseEntity.ok(CollectionModel.of(allStudents));
-  }
+        return ResponseEntity.ok(CollectionModel.of(allStudents));
+    }
 
-  @GetMapping("/{id}/orders")
-  public ResponseEntity<CollectionModel<EntityModel<OrderDTO>>> getOrders(@PathVariable("id") Long id) {
-    StudentEntity student = studentsRepository.
-        findById(id).orElseThrow();
+    @GetMapping("/{id}/orders")
+    public ResponseEntity<CollectionModel<EntityModel<OrderDTO>>> getOrders(@PathVariable("id") Long id) {
+        StudentEntity student = studentsRepository.
+                findById(id).orElseThrow();
 
-    List<EntityModel<OrderDTO>> orders = student.
-        getOrders().
-        stream().
-        map(this::map).
-        map(EntityModel::of).
-        collect(Collectors.toList());
+        List<EntityModel<OrderDTO>> orders = student.
+                getOrders().
+                stream().
 
-    return ResponseEntity.ok(CollectionModel.of(orders));
-  }
+                // map(this::map)
+                // change with orderMapper
+                        map(orderMapper::mapEntityToDTO).
 
-  private OrderDTO map(OrderEntity order) {
-    return new OrderDTO().setId(order.getId()).setStudentId(order.getStudent().getId()).
-        setCourseId(order.getCourse().getId());
-  }
+                map(EntityModel::of).
+                collect(Collectors.toList());
 
-  @GetMapping("/{id}")
-  public ResponseEntity<EntityModel<StudentDTO>> getStudentsByID(@PathVariable("id") Long id) {
-    StudentDTO student = studentsRepository.
-        findById(id).
-        map(studentMapper::mapEntityToDTO).
-        orElseThrow();
+        return ResponseEntity.ok(CollectionModel.of(orders));
+    }
 
-    return ResponseEntity.ok(
-        EntityModel.of(student, createStudentLinks(student))
-    );
-  }
+    private OrderDTO map(OrderEntity order) {
+        return new OrderDTO().setId(order.getId()).setStudentId(order.getStudent().getId()).
+                setCourseId(order.getCourse().getId());
+    }
 
-  @PutMapping("/{id}")
-  public ResponseEntity<EntityModel<StudentDTO>> update(
-      @PathVariable Long id,
-      StudentDTO studentDTO) {
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<StudentDTO>> getStudentsByID(@PathVariable("id") Long id) {
+        StudentDTO student = studentsRepository.
+                findById(id).
+                map(studentMapper::mapEntityToDTO).
+                orElseThrow();
 
-    //IMPLEMENTATION NOT IMPORTANT for the demo
+        return ResponseEntity.ok(
+                EntityModel.of(student, createStudentLinks(student))
+        );
+    }
 
-    return ResponseEntity.ok().build();
-  }
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<StudentDTO>> update(
+            @PathVariable Long id,
+            StudentDTO studentDTO) {
 
-  private Link[] createStudentLinks(StudentDTO studentDTO) {
-    List<Link> result = new ArrayList<>();
+        //IMPLEMENTATION NOT IMPORTANT for the demo
 
-    Link selfLink = linkTo(methodOn(StudentsController.class).
-        getStudentsByID(studentDTO.getId())).withSelfRel();
-    result.add(selfLink);
+        return ResponseEntity.ok().build();
+    }
 
-    Link updateLink = linkTo(methodOn(StudentsController.class).
-        update(studentDTO.getId(), studentDTO)).withRel("update");
-    result.add(updateLink);
+    private Link[] createStudentLinks(StudentDTO studentDTO) {
+        List<Link> result = new ArrayList<>();
 
-    Link orderLink = linkTo(methodOn(StudentsController.class).
-        getOrders(studentDTO.getId())).withRel("orders");
-    result.add(orderLink);
+        Link selfLink = linkTo(methodOn(StudentsController.class).
+                getStudentsByID(studentDTO.getId())).withSelfRel();
+        result.add(selfLink);
 
-    return result.toArray(new Link[0]);
-  }
+        Link updateLink = linkTo(methodOn(StudentsController.class).
+                update(studentDTO.getId(), studentDTO)).withRel("update");
+        result.add(updateLink);
+
+        Link orderLink = linkTo(methodOn(StudentsController.class).
+                getOrders(studentDTO.getId())).withRel("orders");
+        result.add(orderLink);
+
+        return result.toArray(new Link[0]);
+    }
 
 }
